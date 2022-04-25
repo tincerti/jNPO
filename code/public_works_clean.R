a7cedba9b88f3396a3f9bfdf83b510607045b8d9
Trevor Incerti <trevorinc@gmail.com>
2022-04-25 15:24:23 -0400

    Update public_works_clean.R to incorporate new files

--- code/public_works_clean.R
+++ code/public_works_clean.R
@@ -36,6 +36,7 @@ admin_division = syms(grep("都道府県所管の区分|都道府県認定の区
 grantee_jcn = syms(c("法人番号", "契約の相手方の法人番号"))
 npo_type = syms(grep("公益法人の区分", names(pw), value = TRUE))
 contract_reason = syms(grep("随意契約によることとした会計法令|随意契約によることとした業務方法書又は会計規定等の根拠規定及び理由", names(pw), value = TRUE))
+govt_reemployees = syms(grep("再就職の役員の数", names(pw), value = TRUE))
 notes = syms(grep("備　　考|備考|備　考", names(pw), value = TRUE))
 
 # Combine columns based on string matches above
@@ -53,6 +54,7 @@ pw <- pw %>%
     admin_division = coalesce(!!! admin_division),
     grantee_jcn = coalesce(!!! grantee_jcn),
     contract_reason = coalesce(!!! contract_reason),
+    govt_reemployees = coalesce(!!! govt_reemployees),
     npo_type = coalesce(!!! npo_type),
     notes = coalesce(!!! notes)
   ) %>%
@@ -63,7 +65,7 @@ pw <- pw %>%
     -matches("備　　考|備考|備　考|予定価格|契約金額|一般競争入札・指名競争入札の別"),
     -matches("支出元独立行政法人の名称及び法人番号|都道府県|契約の相手方の法人番号"),
     -matches("支出元独立行政法人の名称|応札・応募者数|公益法人の区分"),
-    -matches("随意契約によることとした会計法令"),
+    -matches("随意契約によることとした会計法令|再就職の役員の数"),
     -matches("随意契約によることとした業務方法書又は会計規定等の根拠規定及び理由"),
     -"法人番号", -"公共工事の名称、場所、期間及び種別", -"物品役務等の名称及び数量", 
     -"公共工事の名称、場所、\r\n期間及び種別", -"契約の相手方の商号又は\r\n名称及び住所"
@@ -73,8 +75,7 @@ pw <- pw %>%
     granter_agency = "支出元独立行政法人",
     grant_date = "契約を締結した日",
     est_actual_ratio = "落札率",
-    granter_jcn = "支出元独立行政法人の法人番号",
-    govt_reemployees = "再就職の役員の数",
+    granter_jcn = "支出元独立行政法人の法人番号"
   )
 colnames(pw)
 
@@ -91,14 +92,21 @@ pw <- pw %>% filter(!is.na(contract_amount))
 pw <- pw %>%
   mutate(
     grantee_clean = ifelse(is.na(grantee), grantee_detail, grantee),
-    grantee_clean = str_remove(grantee_clean, ".*法人"),
+    grantee_clean = gsub("\\s*\\（[^\\）]+\\）", "" , grantee_clean), # Remove information in parens
+    # Extract string to the right of first occurance of 法人
+    grantee_clean = sub(".*?法人", "", grantee_clean), 
+    grantee_clean = str_trim(grantee_clean), 
     grantee_detail_clean = str_remove(grantee_clean, ".*法人"),
     grantee_clean = ifelse(is.na(grantee), sub("\\s.*", "", grantee_detail_clean),
-                                               grantee_clean)
-  ) %>%
+                                               grantee_clean),
+    # Manual name cleaning
+    grantee_clean = case_when(
+      grepl("リバーフロント研究所", grantee_clean) ~ "リバーフロント研究所",
+      grepl("日本測量調査技術協会", grantee_clean) ~ "日本測量調査技術協会",
+      TRUE ~ grantee_clean
+    )
+  ) %>% 
   select(-grantee_detail_clean)
-
-check <- pw %>% filter(is.na(grantee)) %>% select(grantee_clean, grantee, grantee_detail)
 
 # Format dates as dates --------------------------------------------------------
 # Add month and year variables
@@ -118,7 +126,7 @@ pw <- pw %>%
 pw <- pw %>%
   mutate(
     contract_amount = as.numeric(contract_amount),
-    contract_amount_est = as.numeric(contract_amount)
+    contract_amount_est = as.numeric(contract_amount_est)
   )
 
 # Add indicator for type of bidding procedure ----------------------------------
@@ -134,7 +142,7 @@ pw <- pw %>%
   mutate(
     govt_reemployees = case_when(
       competitive_bid == "Competitive" ~ "-99",
-      govt_reemployees == "－" ~ "0",
+      govt_reemployees == "－" | govt_reemployees == "-" ~ "0",
       is.na(govt_reemployees) & competitive_bid == "Negotiated" ~ "0",
       TRUE ~ govt_reemployees),
     govt_reemployees = str_remove(govt_reemployees, "名")
@@ -151,7 +159,7 @@ pw <- pw %>%
   arrange(grant_date, granter_ministry, grantee_clean)
 
 # Export to CSV
-write_csv(pw, "data/npo/public_works_clean.csv")
+write_csv(pw, "data/public_works_clean.csv")
 
 # Expand into time series dataset ----------------------------------------------
 pw_ts <- pw %>%
